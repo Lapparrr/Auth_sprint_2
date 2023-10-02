c155e2f16758b209f054ba478fbf169ccc991528
ceraruz <>
2023-10-02 05:31:59 +0300

    add jaeger tracing

--- auth-service/app/src/main.py
+++ auth-service/app/src/main.py
@@ -2,7 +2,7 @@ import logging
 
 import uvicorn
 from async_fastapi_jwt_auth import AuthJWT
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request, status
 from fastapi.responses import ORJSONResponse
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
@@ -24,6 +24,15 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     dependencies=[Depends(RateLimiter(times=10, seconds=25))],
 )
+
+
+@app.middleware('http')
+async def before_request(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    return response
 
 
 @AuthJWT.load_config
